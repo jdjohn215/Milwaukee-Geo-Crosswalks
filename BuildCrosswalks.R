a6239b355d6e532dd626b942db8c2b1cb7a330de
John D. Johnson <jdjohn215@gmail.com>
2019-12-18 11:17:35 -0600

    add blockgroups

--- BuildCrosswalks.R
+++ BuildCrosswalks.R
@@ -12,6 +12,7 @@ neighborhoods <- readRDS("SourceData/MilwaukeeNeighborhoods.rds")
 tracts <- readRDS("SourceData/MilwaukeeCensusTracts2017.rds")
 blocks <- readRDS("SourceData/MilwaukeeCensusBlocks2010.rds")
 wards <- readRDS("SourceData/MilwaukeeWards2018.rds")
+blockgroups <- readRDS("SourceData/MilwaukeeBlockGroups2017.rds")
 
 # 1. select residential parcels
 # 2. reduce to centroids
@@ -25,7 +26,8 @@ residential <- parcels %>%
   st_intersection(neighborhoods) %>%
   st_intersection(tracts) %>%
   st_intersection(wards) %>%
-  st_intersection(blocks)
+  st_intersection(blocks) %>%
+  st_intersection(blockgroups)
 
 
 # this function creates a crosswalk between two geographies
@@ -56,8 +58,16 @@ wards.to.neighborhood <- make.crosswalk(target_geo = neighborhood, data_geo = wa
 blocks.to.wards <- make.crosswalk(target_geo = ward, data_geo = block) %>%
   rename(pct.of.ward = pct.of.target.geo,
          pct.of.block = pct.of.data.geo)
+blockgroups.to.wards <- make.crosswalk(target_geo = ward, data_geo = blockgroup) %>%
+  rename(pct.of.ward = pct.of.target.geo,
+         pct.of.blockgroup = pct.of.data.geo)
+blocksgroups.to.neighborhood <- make.crosswalk(target_geo = neighborhood, data_geo = blockgroup) %>%
+  rename(pct.of.neighborhood = pct.of.target.geo,
+         pct.of.blockgroup = pct.of.data.geo)
 
 write_csv(tracts.to.neighborhood, "Crosswalks/2017CensusTracts_to_Neighborhoods.csv")
 write_csv(blocks.to.neighborhood, "Crosswalks/2010CensusBlocks_to_Neighborhoods.csv")
 write_csv(wards.to.neighborhood, "Crosswalks/2018VotingWards_to_Neighborhoods.csv")
 write_csv(blocks.to.wards, "Crosswalks/2010CensusBlocks_to_2018VotingWards.csv")
+write_csv(blockgroups.to.wards, "Crosswalks/2017CensusBlockGroups_to_2018VotingWards.csv")
+write_csv(blocksgroups.to.neighborhood, "Crosswalks/2017CensusBlockGroups_to_Neighborhoods.csv")
